d5a984ac7d14ed80c1099e30b040e2e8be1dd240
ocefpaf <ocefpaf@gmail.com>
2016-08-17 13:39:52 -0300

    Test win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,21 +1,7 @@
-:: Would be nice to include blosc support, but building this with external blosc is currently broken.
-::    See https://github.com/PyTables/PyTables/issues/451
+set HDF5_DIR=%LIBRARY_PREFIX%
+set BZIP2_DIR=%LIBRARY_PREFIX%
 
-
-:: rd /s /q c-blosc
-:: git clone https://github.com/Blosc/c-blosc
-:: cd c-blosc
-:: git checkout v1.7.0
-:: git clone https://github.com/Blosc/hdf5-blosc
-:: move hdf5-blosc\src hdf5
-:: rd /s /q hdf5-blosc
-
-:: cd ..
-
-:: %PYTHON% setup.py install --blosc=%LIBRARY_PREFIX% --hdf5=%LIBRARY_PREFIX% --bzip2=%LIBRARY_PREFIX%
-
-
-:: Proceed without external blosc
-
-%PYTHON% setup.py install --hdf5=%LIBRARY_PREFIX% --bzip2=%LIBRARY_PREFIX% ^
+%PYTHON% setup.py install --hdf5=%LIBRARY_PREFIX% ^
+                          --bzip2=%LIBRARY_PREFIX% ^
                           --single-version-externally-managed --record record.txt
+if errorlevel 1 exit 1
