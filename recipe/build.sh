e2037635d883790713ed59c151f3e1c785a73fd6
Eli Rykoff <erykoff@stanford.edu>
2021-11-28 14:49:56 -0800

    Remove pre-cythonized files so that cython is re-run.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,4 +4,7 @@ export BZIP2_DIR=$PREFIX
 export HDF5_DIR=$PREFIX
 export LZO_DIR=$PREFIX
 
+# Remove the pre-cythonized files which may not be compatible.
+rm -f tables/*.c
+
 $PYTHON -m pip install --no-deps --no-cache-dir --ignore-installed .
