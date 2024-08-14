7f78c2f50e6f81ead34231298ebd75ba3952eceb
Filipe Fernandes <ocefpaf@gmail.com>
2024-08-14 11:56:50 +0200

    regenerate with grayskull

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,4 +11,4 @@ export PYTABLES_NO_BLOSC2_WHEEL=TRUE
 # Remove the pre-cythonized files which may not be compatible.
 rm -f tables/*.c
 
-$PYTHON -m pip install --no-deps --no-cache-dir --ignore-installed .
+$PYTHON -m pip install . -vv --no-deps --no-build-isolation
